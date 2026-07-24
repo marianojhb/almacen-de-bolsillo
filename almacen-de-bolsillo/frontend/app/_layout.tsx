42ba3e5fc97df72c2668c191b95d2d83670ba04c
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-24 18:25:30 -0300

    (frontend): add custom logo

--- almacen-de-bolsillo/frontend/app/_layout.tsx
+++ almacen-de-bolsillo/frontend/app/_layout.tsx
@@ -15,7 +15,6 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <ProductsProvider>
