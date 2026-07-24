eabe65d06f2dad248a159a437f84b620618e8086
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-24 18:36:52 -0300

    (frontend): fix env file for API

--- almacen-de-bolsillo/frontend/constants/api.ts
+++ almacen-de-bolsillo/frontend/constants/api.ts
@@ -1,1 +1,8 @@
-export const API_URL = "http://192.168.0.158:3000";
+// constants/api.ts
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+if (!apiUrl) {
+  console.warn("EXPO_PUBLIC_API_URL is not defined. Using fallback API URL.");
+}
+
+export const API_URL = apiUrl ?? "http://192.168.0.158:3000";
