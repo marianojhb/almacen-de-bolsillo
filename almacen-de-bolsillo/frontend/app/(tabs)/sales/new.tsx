207fdf4b0bd997842a12cbe0e9ba7a6c658cdd34
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-27 10:23:11 -0300

    (sales): prop screen to add new sale

---
+++ almacen-de-bolsillo/frontend/app/(tabs)/sales/new.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+export const NewSaleScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>New Sale Screen</Text>
+    </View>
+  );
+};
+
+export default NewSaleScreen;
