62bb3da483c64d9368fe09d4556811dcedd059f5
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-27 10:23:35 -0300

    (sales): new prop screen to edit sale

---
+++ almacen-de-bolsillo/frontend/app/(tabs)/sales/[id]/edit.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const EditSaleScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Edit Sale Screen</Text>
+    </View>
+  );
+};
+
+export default EditSaleScreen;
