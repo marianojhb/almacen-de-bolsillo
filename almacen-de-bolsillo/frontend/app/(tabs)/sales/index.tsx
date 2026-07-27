7a2792b1df66d23d17ae1f2f7f8b7425e7604570
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-27 10:29:52 -0300

    (sales): main screen in-progress

--- almacen-de-bolsillo/frontend/app/(tabs)/sales/index.tsx
+++ almacen-de-bolsillo/frontend/app/(tabs)/sales/index.tsx
@@ -1,45 +1,54 @@
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { SalesOrderWithItems } from "@/types/Sales-Order";
+import { Pressable, Text, View, FlatList } from "react-native";
 export default function SalesScreen() {
-  const [sales, setSales] = useState([
-    { id: 1, total: 10000 },
-    { id: 2, total: 20000 },
-    { id: 3, total: 30000 },
-  ]);
+  const [sales, setSales] = useState<SalesOrderWithItems[]>([]);
+  const [totalSales, setTotalSales] = useState<number>(0);
+
+  useEffect(() => {
+    async function fetchSales() {
+      const salesFetched = await fetch("http://192.168.0.158:3000/sales-orders/");
+      const data = await salesFetched.json();
+      setSales(data);
+      setTotalSales(data.reduce((acc: number, sale: SalesOrderWithItems) => acc + Number(sale.total), 0));
+    }
+    fetchSales();
+  }, []);
 
   return (
     <View className="flex-1 p-4">
       <View className="flex-row items-center justify-between mb-4">
-        <Text className="text-3xl font-bold">Listado de ventas...</Text>
+        <Text className="text-3xl font-bold">Listado de ventas</Text>
       </View>
 
-      <View className="gap-2">
-        {sales.map((sale) => (
-          <Pressable key={sale.id} className="flex-row">
-            <Text className="me-3">Venta Nº{sale.id}</Text>
-            <Text className="flex-1" numberOfLines={1} ellipsizeMode="clip">
-              ..................................................................
-            </Text>
-            <Text className="ms-3">${sale.total.toLocaleString()}</Text>
-          </Pressable>
-        ))}
-      </View>
-
-      <View className="flex-row justify-evenly gap-2 mt-4">
-        <Pressable
-          onPress={() => {
-            console.log("Nueva venta");
-          }}
-          className="flex w-24 p-4 items-center justify-center bg-green-600 rounded-lg mt-4">
-          <Text className="text-center text-white text-lg">Nueva venta</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => {
-            console.log(`Nuevo gasto ${new Date().toLocaleTimeString()}`);
-          }}
-          className="flex w-24 p-4 items-center justify-center bg-red-600 rounded-lg mt-4">
-          <Text className="text-center text-white text-lg">Nuevo gasto</Text>
-        </Pressable>
+      <View className="flex-1">
+        <FlatList
+          ItemSeparatorComponent={() => <View className="h-2" />}
+          data={sales}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item: sale }) => (
+            <Pressable key={sale.id} className="flex-row bg-blue-500 p-2 rounded-lg items-center">
+              <Text className=" text-white text-sm me-3">Venta Nº{sale.id}</Text>
+              <Text className="flex-1  text-white text-sm" numberOfLines={1} ellipsizeMode="clip">
+                ..................................................................
+              </Text>
+              <Text className="ms-3  text-white text-sm">
+                {new Date(sale.createdAt).toLocaleDateString("es-AR", {
+                  day: "2-digit",
+                  month: "2-digit",
+                })}
+              </Text>
+              <Text className="ms-3  text-white text-sm">
+                {new Date(sale.createdAt).toLocaleTimeString("es-AR", { hour: "numeric", minute: "numeric" })}
+              </Text>
+              <Text className="ms-3  text-white text-sm">{sale.invoice}</Text>
+              <Text className="ms-3  text-white text-sm">
+                {sale.paymentMethod === "EFECTIVO" ? "EFT" : sale.paymentMethod === "MERCADOPAGO" ? "MP" : "UAL"}
+              </Text>
+              <Text className="ms-3  text-white text-sm">${sale.total.toLocaleString("es-AR")}</Text>
+            </Pressable>
+          )}
+        />
       </View>
     </View>
   );
