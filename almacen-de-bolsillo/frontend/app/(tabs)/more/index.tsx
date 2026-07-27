eb2731526fc991b4b8791f3ed1224ef90d4c0def
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-27 10:21:47 -0300

    (more): prop screen

--- almacen-de-bolsillo/frontend/app/(tabs)/more/index.tsx
+++ almacen-de-bolsillo/frontend/app/(tabs)/more/index.tsx
@@ -1,3 +1,8 @@
-export default function TabTwoScreen() {
-  return <></>;
+import { View, Text } from "react-native";
+export default function MoreScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>More Screen</Text>
+    </View>
+  );
 }
