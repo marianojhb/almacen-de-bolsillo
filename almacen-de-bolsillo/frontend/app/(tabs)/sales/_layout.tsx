f9be9aeefd54048ab7ba30b8e6e50eefc44cff20
Mariano Belgrano <mbelgrano@gmail.com>
2026-07-29 11:05:37 -0300

    (sales): updated layout to include new sales

--- almacen-de-bolsillo/frontend/app/(tabs)/sales/_layout.tsx
+++ almacen-de-bolsillo/frontend/app/(tabs)/sales/_layout.tsx
@@ -7,8 +7,10 @@ export default function SalesLayout() {
     <Stack screenOptions={{ headerBackButtonDisplayMode: "minimal" }}>
       <Stack.Screen name="index" options={{ title: "Ventas" }} />
 
-      <Stack.Screen name="new" options={{ title: "Nueva venta" }} />
+      <Stack.Screen name="new/index" options={{ title: "Nueva venta" }} />
 
+      <Stack.Screen name="new/select-products" options={{ title: "Seleccionar productos", presentation: "modal", headerShown: false }} />
+      
       <Stack.Screen name="[id]/index" options={{ title: "Detalle de la venta" }} />
 
       <Stack.Screen name="[id]/edit" options={{ title: "Editar venta" }} />
